48dc658e7f99292ee6fe03edae1c7a9c00950fac
bigantal <bigantal@users.noreply.github.com>
2022-01-19 10:57:08 +0100

    serve static

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
+app.use(express.static('static'))
 
 app.listen(port, (err) => {
   if (err) {
