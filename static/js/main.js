bf06bf35b1116737af41d78e6c08710e0f41a884
bigantal <bigantal@users.noreply.github.com>
2022-01-19 11:35:19 +0100

    stylized clock

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,24 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js');
+  }
+
+
+  updateTime();
+  setInterval(() => {
+    updateTime();
+  }, 1000);
+
+}
+
+let timeStr = "";
+function updateTime() {
+  const time = new Date();
+  const timeStr2 = `${time.getHours()}:${time.getMinutes()}`;
+  if (timeStr !== timeStr2) {
+    timeStr = timeStr2;
+    const clockEl=document.getElementsByClassName("clock")[0];
+    clockEl.innerHTML = timeStr;
   }
 }
